9e0a92270cbb88198a231e9688ca30306744f6ee
Tom Humphris <tom@muska.co.uk>
2018-02-09 23:39:06 +0000

    make work and stuff and better

--- index.js
+++ index.js
@@ -5,26 +5,35 @@ const config = require('./config.json')
 
 const URI = config.URI
 const TEXT_BELT_KEY = config.SMS_KEY
-const PHONE_NUMBER = config.PHONE_NUMBER
+const PHONE_NUMBERS = config.PHONE_NUMBERS
 const INTERVAL = config.INTERVAL_MS
 
-const success = () => {
-  console.log('BUY DI TIKITZ!!!')
-
+const sendText = (number) => {
   axios.post('https://textbelt.com/text', {
-    phone: `+${PHONE_NUMBER}`,
+    phone: number,
     message: `BUY DI TIKITZ!!!`,
     key: TEXT_BELT_KEY,
   })
     .then(response => {
       if (response.data.success) {
-        process.exit()
+        console.log(`SMS sent: ${number}`)
       }
-      
     })
     .catch(error => {
       console.warn(error)
-    })
+    })  
+}
+
+const success = async () => {
+  console.log('BUY DI TIKITZ!!!')
+
+  PHONE_NUMBERS.forEach(number => {
+    sendText(number)
+  })
+
+  setTimeout(() => {
+    process.exit()
+  }, 5000)
 }
 
 const failure = () => {
